46da4bcc3839c623dfa15a747e16bbc7402c68f0
guanghechen <me.guanghechen@gmail.com>
2022-01-19 23:59:22 +0800

    :arrow_up:  chore: upgrade dependencies

--- packages/binary-index-tree/__test__/binary-saerch-tree.spec.ts
+++ packages/binary-index-tree/__test__/binary-saerch-tree.spec.ts
@@ -45,7 +45,7 @@ describe('tree1', function () {
     const bit = createBinaryIndexTree1<bigint>(0n)
     bit.init(MAX_N)
 
-    const A: Array<bigint> = new Array(MAX_N)
+    const A: bigint[] = new Array(MAX_N)
       .fill(0)
       .map((_x, i) => BigInt(randomInt(MAX_N) + 17))
     for (let x = 0; x < MAX_N; ++x) bit.add(x + 1, A[x])
@@ -117,7 +117,7 @@ describe('tree2', function () {
     bit.init(MAX_N)
 
     // Initialize
-    const A: Array<bigint> = new Array(MAX_N)
+    const A: bigint[] = new Array(MAX_N)
       .fill(0)
       .map((_x, i) => BigInt(randomInt(MAX_N) + 17))
     for (let x = 0; x < MAX_N; ++x) {
@@ -188,7 +188,7 @@ describe('tree1-mod', function () {
     const bit = createBinaryIndexTree1Mod<bigint>(0n, BigInt(MOD))
     bit.init(MAX_N)
 
-    const A: Array<bigint> = new Array(MAX_N)
+    const A: bigint[] = new Array(MAX_N)
       .fill(0)
       .map((_x, i) => BigInt(randomInt(MOD)))
     for (let x = 0; x < MAX_N; ++x) bit.add(x + 1, A[x])
@@ -261,7 +261,7 @@ describe('tree2-mod', function () {
     bit.init(MAX_N)
 
     // Initialize
-    const A: Array<bigint> = new Array(MAX_N)
+    const A: bigint[] = new Array(MAX_N)
       .fill(0)
       .map((_x, i) => BigInt(randomInt(MOD - 1)))
     for (let x = 0; x < MAX_N; ++x) {
