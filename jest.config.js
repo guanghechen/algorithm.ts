26f92fc8c6b22d616006efa469ebcc17512775ca
guanghechen <me.guanghechen@gmail.com>
2022-03-20 22:42:43 +0800

    :wrench:  chore: update coverageThreshold

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,9 @@ module.exports = {
   ...baseConfig,
   coverageThreshold: {
     global: {
-      branches: 95,
-      functions: 100,
-      lines: 100,
+      branches: 90,
+      functions: 95,
+      lines: 95,
       statements: 95,
     },
   },
