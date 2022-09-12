6d2a140fbb18b67cf97bcc742a2c2844c6ae6874
guanghechen <me.guanghechen@gmail.com>
2022-09-12 23:35:12 +0800

    Don't build constant as external module

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,26 @@
 import createRollupConfig from '@guanghechen/rollup-config'
 import path from 'path'
 
-async function rollupConfig() {
+const internalModules = new Set(['@algorithm.ts/constant'])
+
+export default async function rollupConfig() {
   const { default: manifest } = await import(path.resolve('package.json'))
-  const config = createRollupConfig({
+  const configs = createRollupConfig({
     manifest,
     pluginOptions: {
       typescriptOptions: { tsconfig: 'tsconfig.src.json' },
     },
   })
-  return config
-}
 
-export default rollupConfig()
+  configs.forEach(config => {
+    if (config.external) {
+      const external = config.external
+      // eslint-disable-next-line no-param-reassign
+      config.external = function (id) {
+        if (internalModules.has(id)) return false
+        return external(id)
+      }
+    }
+  })
+  return configs
+}
