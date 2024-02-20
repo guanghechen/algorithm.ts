e7fa1259ef9f9861f5e0526859401e450bd5d0a1
guanghechen <me.guanghechen@gmail.com>
2024-02-21 02:13:57 +0800

    :arrow_up:  chore: upgrade devDependencies

--- jest.config.mjs
+++ jest.config.mjs
@@ -4,25 +4,27 @@ import url from 'node:url'
 
 export default async function () {
   const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
-  const baseConfig = await tsMonorepoConfig(__dirname, { useESM: true })
   const { default: manifest } = await import(path.resolve('package.json'), {
     assert: { type: 'json' },
   })
+  const baseConfig = await tsMonorepoConfig(__dirname, { useESM: true })
 
   const config = {
     ...baseConfig,
-    preset: 'ts-jest/presets/default-esm',
-    coverageProvider: 'babel',
     coverageThreshold: {
+      ...coverageMap[manifest.name],
+
       global: {
         branches: 100,
         functions: 100,
         lines: 100,
         statements: 100,
-        ...manifest.jest?.coverageThreshold?.global,
+        ...coverageMap[manifest.name]?.global,
       },
     },
     extensionsToTreatAsEsm: ['.ts', '.mts'],
   }
   return config
 }
+
+const coverageMap = {}
