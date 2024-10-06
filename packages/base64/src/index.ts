a3b2e2a222511234de258e0ec636f269df455f0a
guanghechen <me.guanghechen@gmail.com>
2024-10-06 10:47:41 +0800

    :arrow_up:  chore: upgrade dependencies

--- packages/base64/src/index.ts
+++ packages/base64/src/index.ts
@@ -8,5 +8,4 @@ export const encode = base64.encode.bind(base64)
 export const decode = base64.decode.bind(base64)
 export const validate = base64.validate.bind(base64)
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default { encode, decode, validate }
