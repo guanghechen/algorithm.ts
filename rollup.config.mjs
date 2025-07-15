7beb9cde11d72dc2dd1d8424106af7af4f4cef1b
guanghechen <me.guanghechen@gmail.com>
2025-07-15 22:53:54 +0800

    :wrench: chore: upgrade devDependencies & fix build config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -20,6 +20,9 @@ export default async function rollupConfig() {
       tsPresetConfigBuilder({
         typescriptOptions: {
           tsconfig: 'tsconfig.lib.json',
+          compilerOptions: {
+            moduleResolution: 'node',
+          },
         },
         additionalPlugins: [modify()],
       }),
